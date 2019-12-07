970eda83938460a2a216e78edb1743f18382f9f3
Emil Hegyi <hegyi.emil.peter@gmail.com>
2019-12-07 15:01:20 +0100

    Day 7 / 1 / beta solution

---
+++ AdventOfCode2019.Core/Day7.cs
@@ -0,0 +1,233 @@
+﻿using AdventOfCode2019.InputReader;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2019.Core
+{
+    public class Day7
+    {
+        List<int> testResult = new List<int>();
+
+        public int GetuOutput(string path, int input)
+        {
+            Day2InputReader day2InputReader = new Day2InputReader();
+            int[] fileContent = day2InputReader.ReadArray(path, ',');
+
+            int maximum = 0;
+
+            int[] phase = new int[5] { 0, 0, 0, 0, 0 };
+            for (int i = 0; i <= 44444; i++)
+            {
+                string number = GetNumberAsString(i);
+                if (!NumberContains01234(number))
+                {
+                    continue;
+                }
+
+                for (int j = 0; j < 5; j++)
+                {
+                    phase[j] = int.Parse(number[j].ToString());
+                }
+
+                int output = GetOutputForPhase(fileContent, phase);
+                Debug.WriteLine($"Found: {number} -> {output}");
+                if (output > maximum)
+                {
+                    maximum = output;
+                    Debug.WriteLine($"MAX: {number} -> {output}");
+                }
+            }
+            return maximum;
+        }
+        public int GetOutputForPhase(string path, int[] phases)
+        {
+            Day2InputReader day2InputReader = new Day2InputReader();
+            int[] fileContent = day2InputReader.ReadArray(path, ',');
+
+            return GetOutputForPhase(fileContent, phases);
+        }
+        private int GetOutputForPhase(int[] fileContent, int[] phases)
+        {
+            var amplifierA = IntCode(fileContent, new int[] { phases[0], 0 });
+            var amplifierB = IntCode(fileContent, new int[] { phases[1], amplifierA });
+            var amplifierC = IntCode(fileContent, new int[] { phases[2], amplifierB });
+            var amplifierD = IntCode(fileContent, new int[] { phases[3], amplifierC });
+            var amplifierE = IntCode(fileContent, new int[] { phases[4], amplifierD });
+            return amplifierE;
+        }
+
+        private bool NumberContains01234(string input)
+        {
+            return input.All(c => c == '0' || c == '1' || c == '2' || c == '3' || c == '4')
+                && input.Count(c => c == '0') == 1
+                && input.Count(c => c == '1') == 1
+                && input.Count(c => c == '2') == 1
+                && input.Count(c => c == '3') == 1
+                && input.Count(c => c == '4') == 1;
+        }
+
+        private static string GetNumberAsString(int i)
+        {
+            var numberAsString = i.ToString();
+            while (numberAsString.Length != 5)
+            {
+                numberAsString = numberAsString.Insert(0, "0");
+            }
+
+            return numberAsString;
+        }
+
+        private int IntCode(int[] input, int[] parameters)
+        {
+            int i = 0;
+            int j = 0;
+            while (i < input.Length)
+            {
+                int opcode = input[i] % 100;
+                int parameterMode1 = (input[i] % 1000) / 100;
+                int parameterMode2 = (input[i] % 10000) / 1000;
+                int parameterMode3 = (input[i] % 100000) / 10000;
+                int parameter1, parameter2, position;
+                switch (opcode)
+                {
+                    case 1:
+                        parameter1 = GetParameter(input, i + 1, parameterMode1);
+                        parameter2 = GetParameter(input, i + 2, parameterMode2);
+                        int sumOfParameters = parameter1 + parameter2;
+                        if (parameterMode3 == 0)
+                        {
+                            input[input[i + 3]] = sumOfParameters;
+                        }
+                        else
+                        {
+                            input[i + 3] = sumOfParameters;
+                        }
+                        i += 4;
+                        break;
+                    case 2:
+                        parameter1 = GetParameter(input, i + 1, parameterMode1);
+                        parameter2 = GetParameter(input, i + 2, parameterMode2);
+                        int multiplyOfParameters = parameter1 * parameter2;
+                        if (parameterMode3 == 0)
+                        {
+                            input[input[i + 3]] = multiplyOfParameters;
+                        }
+                        else
+                        {
+                            input[i + 3] = multiplyOfParameters;
+                        }
+                        i += 4;
+                        break;
+                    case 3:
+                        if (parameterMode1 == 0)
+                        {
+                            input[input[i + 1]] = parameters[j];
+                        }
+                        else
+                        {
+                            input[i + 1] = parameters[j];
+                        }
+                        i += 2;
+                        j++;
+                        break;
+                    case 4:
+                        if (parameterMode1 == 0)
+                        {
+                            testResult.Add(input[input[i + 1]]);
+                        }
+                        else
+                        {
+                            testResult.Add(input[i + 1]);
+                        }
+                        i += 2;
+                        break;
+                    case 5:
+                        parameter1 = GetParameter(input, i + 1, parameterMode1);
+                        if (parameter1 != 0)
+                        {
+                            i = GetParameter(input, i + 2, parameterMode2);
+                        }
+                        else
+                        {
+                            i += 3;
+                        }
+                        break;
+                    case 6:
+                        parameter1 = GetParameter(input, i + 1, parameterMode1);
+                        if (parameter1 == 0)
+                        {
+                            i = GetParameter(input, i + 2, parameterMode2);
+                        }
+                        else
+                        {
+                            i += 3;
+                        }
+                        break;
+                    case 7:
+                        parameter1 = GetParameter(input, i + 1, parameterMode1);
+                        parameter2 = GetParameter(input, i + 2, parameterMode2);
+                        position = parameterMode3 == 0 ? input[i + 3] : input[input[i + 3]];
+                        if (parameter1 < parameter2)
+                        {
+                            input[position] = 1;
+                        }
+                        else
+                        {
+                            input[position] = 0;
+                        }
+                        i += 4;
+                        break;
+                    case 8:
+                        int nextParameter41 = GetParameter(input, i + 1, parameterMode1);
+                        int nextParameter42 = GetParameter(input, i + 2, parameterMode2);
+                        position = parameterMode3 == 0 ? input[i + 3] : input[input[i + 3]];
+                        if (nextParameter41 == nextParameter42)
+                        {
+                            input[position] = 1;
+                        }
+                        else
+                        {
+                            input[position] = 0;
+                        }
+                        i += 4;
+                        break;
+                    case 99:
+                        int response = testResult.Last();
+                        testResult[testResult.Count - 1] = 0;
+                        if (!testResult.All(x => x == 0))
+                        {
+                            throw new Exception();
+                        }
+
+                        return response;
+                    default:
+                        throw new Exception();
+                }
+            }
+
+            throw new Exception();
+        }
+
+        private static void WriteInputToDebug(int[] input)
+        {
+            StringBuilder sb = new StringBuilder("VALUES: ");
+            for (int t = 0; t < input.Length; t++)
+            {
+                sb.Append(input[t] + " ");
+            }
+
+            Debug.WriteLine(sb.ToString());
+        }
+
+        private static int GetParameter(int[] input, int i, int mode)
+        {
+            if (mode == 0)
+                return input[input[i]];
+
+            return input[i];
+        }
+    }
+}
---
+++ AdventOfCode2019.Tests/Day7Tests.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCode2019.Core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2019.Tests
+{
+    public class Day7Tests
+    {
+        [Test]
+        [TestCase(@"..\..\..\Inputs\day7_demo1.txt", new int[] { 4,3,2,1,0 }, ExpectedResult = 43210)]
+        [TestCase(@"..\..\..\Inputs\day7_demo2.txt", new int[] { 0,1,2,3,4 }, ExpectedResult = 54321)]
+        [TestCase(@"..\..\..\Inputs\day7_demo3.txt", new int[] { 1,0,4,3,2 }, ExpectedResult = 65210)]
+        public int GetMaximumThrusterSignal(string path, int[] phase)
+        {
+            Day7 day7_solution = new Day7();
+            return day7_solution.GetuOutput(path, 0);
+        }
+
+        [Test]
+        public void FirstStarTest()
+        {
+            Day7 day7_solution = new Day7();
+            var response = day7_solution.GetuOutput(@"..\..\..\Inputs\day7_1.txt", 0);
+            Assert.AreEqual(0, response);
+        }
+    }
+}
